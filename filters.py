2d3034adc2afa8f485f1732385c99f2d72c8c671
firewheel996 <ianj.foreman@gmail.com>
2014-01-10 12:22:41 -0500

    Updated filters.py to use hsv colorspace when filtering. TODO: Add in image blurring or discover if it is needed

--- filters.py
+++ filters.py
@@ -1,6 +1,9 @@
 #! usr/bin/python
-import numpy as np
-import cv2
+import cv
+
+global redmin, redmax
+redmin = cv.Scalar(40,0,0)
+redmax = cv.Scalar(160,100,100)
 
 """ Isolate Color red in a picture
 Args:
@@ -8,17 +11,39 @@ img: Image to be processed, can be in either format
 blur: boolean whether to apply a blur on the image default: True
 minRed: (Optional) Minimum color red to be filtered Default: 37
 Returns Binary filtered image"""
-def isoRed(img, blur=True, minRed=37):
-	if(type(img).__module__ == np.__name__):
-		working = np.asarray(img[:,:])
+def isoRed(img, blur=True):
+	if(type(img).__module__ == cv.__name__):
+		working = cv.fromarray(img)
 	else:
-		working = np.copy(img)
+		working = cv.CloneImage(img)
 
-	if(blur):
-		cv2.blur(working, (2,2), working)
-	
-	iso = cv2.inRange(working, np.array([0,0,minRed]),np.array([10,25,255]))
-	return iso
+	#if(blur):
+	#	cv2.blur(working, (2,2), working)
+
+	imghsv= cv.CreateImage(cv.GetSize(img), 8, 3)
+	imgthresh = cv.CreateImage(cv.GetSize(img),8,1)
+
+	cv.CvtColor(img, imghsv, cv.CV_BGR2HSV)
+	cv.InRangeS(imghsv,redmin, redmax, imgthresh)
+	#cv.AbsDiffS(imgthresh,imgthresh, 255)
+	return imgthresh
+
+def isoRed2(img, hlow, slow, vlow, hhigh, shigh, vhigh):
+	if(type(img).__module__ == cv.__name__):
+		working = cv.fromarray(img)
+	else:
+		working = cv.CloneImage(img)
+
+	#if(blur):
+	#	cv2.blur(working, (2,2), working)
+
+	imghsv= cv.CreateImage(cv.GetSize(img), 8, 3)
+	imgthresh = cv.CreateImage(cv.GetSize(img),8,1)
+
+	cv.CvtColor(img, imghsv, cv.CV_BGR2HSV)
+	cv.InRangeS(imghsv,cv.Scalar(hlow,slow,vlow), cv.Scalar(hhigh,shigh,vhigh), imgthresh)
+	cv.AbsDiffS(imgthresh,imgthresh, 255)
+	return imgthresh
 
 """ Isolate Color blue in a picture
 Args:
@@ -26,13 +51,18 @@ img: Image to be processed, can be in either format
 blur: boolean whether to apply blur on the image default: True
 Returns Binary filtered image"""
 def isoBlue(img, blur=True):
-	if(type(img.__module__ == np.__name__):
-		working = np.asarray(img[:,:])
+	if(type(img).__module__ == cv.__name__):
+		working = cv.fromarray(img)
 	else:
-		working = np.copy(img)
+		working = cv.CloneImage(img)
 
-	if(blur):
-		cv2.blur(working, (2,2), working)
+	#if(blur):
+	#	cv2.blur(working, (2,2), working)
 
-	iso = cv2.inRange(working, np.array([0,20,20]), np.array([255,50,50]))
-	return iso
+	imghsv= cv.CreateImage(cv.GetSize(img), 8, 3)
+	imgthresh = cv.CreateImage(cv.GetSize(img),8,1)
+
+	cv.CvtColor(img, imghsv, cv.CV_BGR2HSV)
+	cv.InRangeS(imghsv, cv.Scalar(90,50,50), cv.Scalar(120,255,255), imgthresh)
+	
+	return imgthresh
