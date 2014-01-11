d0e52b4a95b345f362bc7b1eebfb89b2ce5fc480
BexleyRobotics <brian.davis@bexleyschools.org>
2014-01-11 13:08:28 -0500

    Update findLargestContour.py
    Finds the largest contour in an image

--- findLargestContour.py
+++ findLargestContour.py
@@ -6,8 +6,8 @@ contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPR
 			max = moments['m00']
                         x = int(moments['m10']/moments['m00'])
                         y = int(moments['m01']/moments['m00'])
-                        #cv2.drawContours(display, cnt,1, (0,255,0), 1)
-                        #cv2.circle(display, (x,y),int(math.sqrt(moments['m00']/math.pi)), red,-1)
-                        #cv2.circle(display, (x,y),5,blue,-1)
+                        cv2.drawContours(display, cnt,1, (0,255,0), 1)
+                        cv2.circle(display, (x,y),int(math.sqrt(moments['m00']/math.pi)), red,-1)
+                        cv2.circle(display, (x,y),5,blue,-1)
 			largest = max
 
