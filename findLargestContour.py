4ce2c8b7eb7c7ce077de78bc4eae74b49a0c8e18
firewheel996 <ianj.foreman@gmail.com>
2014-01-11 12:51:33 -0500

    Create findLargestContour.py

---
+++ findLargestContour.py
@@ -0,0 +1,10 @@
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	for cnt in contours:
+		moments = cv2.moments(cnt)
+		if moments['m00'] > 1000:
+			x = int(moments['m10']/moments['m00'])
+			y = int(moments['m01']/moments['m00'])
+			cv2.drawContours(display, cnt,1, (0,255,0), 1)
+			cv2.circle(display, (x,y),int(math.sqrt(moments['m00']/math.pi)), red,-1)
+			cv2.circle(display, (x,y),5,blue,-1)
+	
